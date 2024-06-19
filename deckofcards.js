75f16112ea19894c8019af38c7b8f1d05e4a40ba
Clay O'Dell <clay.odell.music@gmail.com>
2024-06-19 14:46:47 -0500

    parts one and two completed on draw a card

---
+++ deckofcards.js
@@ -0,0 +1,28 @@
+const baseURL = "https://deckofcardsapi.com/api/deck";
+const deck = {
+  async init() {
+    let res = await axios.get(`${baseURL}/new`);
+    this.deckID = res.data.deck_id;
+    console.log(this.deckID);
+  },
+  async shuffle() {
+    let res = await axios.get(`${baseURL}/${this.deckID}/shuffle`);
+    console.log(res.data);
+  },
+  async drawCard() {
+    let res = await axios.get(`${baseURL}/${this.deckID}/draw/?count=1`);
+    let card = res.data.cards[0].image;
+    cardDiv = document.querySelector("#cards");
+    let img = document.createElement("img");
+    img.src = card;
+    cardDiv.append(img);
+  },
+};
+async function partTwo() {
+  await deck.init();
+  await deck.shuffle();
+  await deck.drawCard();
+  await deck.drawCard();
+  console.log(deck);
+}
+partTwo();
