13a95b482731d6bcc4f52aaf2ffda050654694ad
Clay O'Dell <clay.odell.music@gmail.com>
2024-06-19 15:35:51 -0500

    part 3 completed

--- deckofcards.js
+++ deckofcards.js
@@ -12,19 +12,31 @@ const deck = {
   async drawCard() {
     let res = await axios.get(`${baseURL}/${this.deckID}/draw/?count=1`);
     let card = res.data.cards[0].image;
-    cardDiv = document.querySelector("#cards");
+    cardDiv = document.querySelector("#showCard");//changed part two from (#card)
     let img = document.createElement("img");
     img.src = card;
     cardDiv.append(img);
     console.log(res.data.cards[0].code);
   },
 };
-//Draws two cards from a shuffled deck
+//Part 2: draws two cards from a shuffled deck
 async function partTwo() {
   await deck.init();
   await deck.shuffle();
   await deck.drawCard();
   await deck.drawCard();
 }
-partTwo();
+// partTwo();
 
+// Part 3:
+async function partThree() {
+  await deck.init();
+  await deck.shuffle();
+}
+partThree();
+
+let form = document.querySelector('#drawACard form');
+form.addEventListener('submit', async function(evt){
+    evt.preventDefault();
+    await deck.drawCard();
+})
